6b776ff8c6e7360cd5e9c25222462c09fa57ddea
nwdorian <neuwirthdorian@gmail.com>
2026-03-17 19:55:05 +0100

    chore: initial commit

---
+++ src/Web/Program.cs
@@ -0,0 +1,22 @@
+﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}").WithStaticAssets();
+
+await app.RunAsync();
