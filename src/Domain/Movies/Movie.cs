b8c329d54aca23903171235919ee7d16c2d88ceb
nwdorian <118033138+nwdorian@users.noreply.github.com>
2026-03-19 15:06:21 +0100

    feat: add domain models for Movie and Genre entities (#2)

---
+++ src/Domain/Movies/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Movies;
+
+public sealed class Movie
+{
+    public Guid Id { get; set; }
+    public Guid GenreId { get; set; }
+    public required string Title { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public decimal Price { get; set; }
+    public int Rating { get; set; }
+}
