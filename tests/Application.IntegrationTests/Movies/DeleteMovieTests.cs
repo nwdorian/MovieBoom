c7ee401e0604b8d69cb156c6a51ad5cbd4adcf4f
nwdorian <neuwirthdorian@gmail.com>
2026-04-09 15:29:51 +0200

    add tests for Delete method

---
+++ tests/Application.IntegrationTests/Movies/DeleteMovieTests.cs
@@ -0,0 +1,37 @@
+using Application.Abstractions.Movies;
+using Application.IntegrationTests.Core;
+using Application.Movies.Commands;
+using Domain.Common.Results;
+using Domain.Movies;
+using Infrastructure.Movies;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Application.IntegrationTests.Movies;
+
+public class DeleteMovieTests(IntegrationTestWebAppFactory factory) : BaseIntegrationTest(factory)
+{
+    private IMovieService _movieService => Factory.Services.GetRequiredService<IMovieService>();
+
+    [Fact]
+    public async Task Delete_ShouldReturnNotFound_WhenMovieDoesNotExist()
+    {
+        DeleteMovieCommand command = new(Guid.Empty);
+
+        Result result = await _movieService.Delete(command, CancellationToken.None);
+
+        result.IsSuccess.ShouldBeFalse();
+        result.Error.ShouldBeEquivalentTo(MovieErrors.NotFoundById(Guid.Empty));
+    }
+
+    [Fact]
+    public async Task Delete_ShouldReturnSuccess_WhenMovieWasDeleted()
+    {
+        DeleteMovieCommand command = new(MovieFaker.ShawshankId);
+
+        Result result = await _movieService.Delete(command, CancellationToken.None);
+
+        result.IsSuccess.ShouldBeTrue();
+        DbContext.Movies.Any(m => m.Id == MovieFaker.ShawshankId).ShouldBeFalse();
+    }
+}
